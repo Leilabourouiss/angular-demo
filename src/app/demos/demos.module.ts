a76989950e42739b2c0fb00cc60e7d310e40c5fa
Leila <leilabourouiss@hotmail.fr>
2023-02-02 16:10:47 +0100

    exo5

---
+++ src/app/demos/demos.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DemosRoutingModule } from './demos-routing.module';
+import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
+import { Demo3Component } from './demo3/demo3.component';
+import { Demo4Component } from './demo4/demo4.component';
+import { Demo5Component } from './demo5/demo5.component';
+import { ToTemperature } from './pipes/to-temperature.pipe';
+
+
+@NgModule({
+  declarations: [
+    Demo1Component,
+    Demo2Component,
+    Demo3Component,
+    Demo4Component,
+    Demo5Component,
+    ToTemperature
+  ],
+  imports: [
+    CommonModule,
+    DemosRoutingModule
+  ]
+})
+export class DemosModule { }
--- src/app/exos/exo5-cours/exo5-cours.component.ts
+++ src/app/exos/exo5-cours/exo5-cours.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class Exo5CoursComponent {
 
+tabMenu:string []= ['HTML','CSS','JS','C#']
+// isOpen : boolean = false;
+// isOpen2 : boolean = false;
+// isOpen3 : boolean = false;
+// isOpen4 : boolean = false;
+// // 
+
+// toggleisOpen(){
+//   this.isOpen = !this.isOpen;  
+// }
+// toggleisOpen2(){ 
+//   this.isOpen2 = !this.isOpen2;
+// }
+// toggleisOpen3(){ 
+//   this.isOpen3 = !this.isOpen3;
+// }
+// toggleisOpen4(){ 
+//   this.isOpen4 = !this.isOpen4;
+// }
+  
+  Cours: Cours[]= [new Cours("HTML", 'Cours HTML'),
+  new Cours("CSS", "C'est joli"),
+  new Cours("js", 'Les rendre dynamiques'),
+  new Cours("C#", "Un super langage orienté objet")]
+
+}
+
+export class Cours {
+  title: string;
+  description: string;
+  isActive: boolean;
+  constructor(title: string, description:string, isActive: boolean = false) {
+      this.title = title;
+      this.description = description;
+      this.isActive = isActive;
+  }
 }
